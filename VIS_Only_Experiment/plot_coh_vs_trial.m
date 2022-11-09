a78ea52500ffbfca3ff748f96fc3e4a071e5045a
ramlab <ramlab.github@gmail.com>
2022-11-09 13:55:55 -0600

    Fixed Bug in coh vs trial plotter

--- VIS_Only_Experiment/plot_coh_vs_trial.m
+++ VIS_Only_Experiment/plot_coh_vs_trial.m
@@ -1,10 +1,13 @@
 function fig = plot_coh_vs_trial(dataout)
-
+hold on 
     trial_num = cell2mat(dataout(2:end,1));
     coh_level = cell2mat(dataout(2:end,8)); 
-    fig = figure();
+    
+    fig = figure( 'Name', 'Coherence vs Trial Number') ;
     %fig.Position = [850 1240 1550 500];
-    plot(trial_num, coh_level);
+    plot(trial_num, coh_level)
+    
+    
     xlabel('Trial Number');
     ylabel('Coherence Level');
     title('Coherence Level vs. Trial Number')
