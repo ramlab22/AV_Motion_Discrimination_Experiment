027c82eb4fb959fec85d333d9207b2b91a7918d7
Jackson Mayfield <jacksonm.tn@gmail.com>
2023-02-08 15:40:23 -0600

    Fixed typo in graph functions

--- Mixed_Modality_and_AV_Exp/psychometric_plotter_modalities.m
+++ Mixed_Modality_and_AV_Exp/psychometric_plotter_modalities.m
@@ -1,4 +1,9 @@
-function [fig, AUD_p_values, VIS_p_values,AUD_threshold,VIS_threshold] = psychometric_plotter_modalities(AUD_prob_Right, AUD_prob_Left, VIS_prob_Right, VIS_prob_Left, audInfo, dotInfo, chosen_threshold,save_name);
+function [fig, AUD_p_values, VIS_p_values, AUD_threshold, VIS_threshold] =...
+    psychometric_plotter_modalities(AUD_prob_Right, AUD_prob_Left, VIS_prob_Right, VIS_prob_Left,...
+    AV_prob_Right, AV_prob_Left,...
+    audInfo, dotInfo, AVInfo, chosen_threshold, save_name)
+
+
 %PYSCHOMETRIC_PLOTTER Summary of this function goes here
 %  Must take 1-prob_Left to get the probability of a rightward choice, the
 %  input probabilities are in regards to the corrrect choice(i.e. in that
@@ -23,7 +28,10 @@ VIS.y = cat(2,VIS.yL,VIS.yR);
 VIS.plot_data = [VIS.x; VIS.y]'; 
 VIS.plot_data = VIS.plot_data(~isnan(VIS.plot_data(:,2)),:); 
 
-[fig, AUD_p_values, VIS_p_values,AUD_threshold,VIS_threshold] = createFit_NormCDF_modalities(AUD.plot_data(:,1), AUD.plot_data(:,2)/100, VIS.plot_data(:,1), VIS.plot_data(:,2)/100, audInfo, dotInfo, chosen_threshold,save_name); 
+[fig, AUD_p_values, VIS_p_values,AUD_threshold,VIS_threshold] = ...
+    createFit_NormCDF_modalities(AUD.plot_data(:,1), AUD.plot_data(:,2)/100,...
+                                 VIS.plot_data(:,1), VIS.plot_data(:,2)/100,...
+                                 audInfo, dotInfo, chosen_threshold,save_name); 
 
 
 
