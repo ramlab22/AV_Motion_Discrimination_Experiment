5b6923d46cce7ed9cd78ea0063d16f6dd88c389a
ramlab <ramlab.github@gmail.com>
2023-08-15 10:21:15 -0500

    fix vis only mcs

--- VIS_Only_Experiment/dir_randomizer_MCS.m
+++ VIS_Only_Experiment/dir_randomizer_MCS.m
@@ -12,11 +12,11 @@ function [random_dir_list] = dir_randomizer_MCS(ExpInfo,dotInfo)
     random_dir_list = zeros(1,ExpInfo.num_trials);
     %Direction Randomizer
     
-    for c = audInfo.coherences
+    for c = dotInfo.coherences
 
-        if length(audInfo.dirSet) > 1 %If we have more than 1 direction to choose from
+        if length(dotInfo.dirSet) > 1 %If we have more than 1 direction to choose from
 
-            numberOfElements = sum(audInfo.random_coh_list == c); %Sums all of the same coherences from the random list 
+            numberOfElements = sum(dotInfo.random_coh_list == c); %Sums all of the same coherences from the random list 
             per_R = 50; % the percentage of Right Targets, 50% will make an even number of L and R trials
 
             numberOfOnes = round(numberOfElements * per_R / 100);
@@ -24,18 +24,18 @@ function [random_dir_list] = dir_randomizer_MCS(ExpInfo,dotInfo)
             signal = [ones(1, numberOfOnes), zeros(1, numberOfElements - numberOfOnes)];
             % Scramble them up with randperm
             signal = signal(randperm(length(signal)));%For Direction
-            indeces = find(audInfo.random_coh_list == c); 
+            indeces = find(dotInfo.random_coh_list == c); 
             
             for i = 1:numberOfElements
                 if signal(i) == 0
-                     random_dir_list(indeces(i)) = audInfo.dirSet(2); %Leftward wherever that coherence appears 
+                     random_dir_list(indeces(i)) = dotInfo.dirSet(2); %Leftward wherever that coherence appears 
                 elseif signal(i) == 1
-                    random_dir_list(indeces(i)) = audInfo.dirSet(1); %Rightward wherever that coherence appears 
+                    random_dir_list(indeces(i)) = dotInfo.dirSet(1); %Rightward wherever that coherence appears 
                 end
 
             end
         else
-            random_dir_list(:) = audInfo.dirSet;
+            random_dir_list(:) = dotInfo.dirSet;
         end
     end
    
